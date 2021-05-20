e21023a1bd2d557767e079554cf69c65b6e04cbe
Shreyansh Singh <shreyansh071299@gmail.com>
2021-05-20 11:05:12 +0530

    added new views and finished basic css with bootstrap

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
+const articleRouter = require("./routes/articles");
 app.set('view engine', 'ejs');
 const port = 3000
 
+app.use('/articles', articleRouter);
+
 app.get('/', (req, res) => {
-    res.render('home');
+    const articles = [{
+        title: "article title1",
+        created: new Date(),
+        description: "article description1"
+    },
+
+    {
+        title: "article title2",
+        created: new Date(),
+        description: "article description2"
+    }]
+
+    res.render('articles/home', {articles: articles});
 })
 
 app.listen(port, () => {
